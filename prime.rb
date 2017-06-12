8caceb7b66d01cf3d9a0e645b1ffc5087b717f38
yuralala8 <yc1435@nyu.edu>
2017-06-12 17:17:59 -0400

    prime commit

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(integer)
+    array = (2..integer-1).to_a
+    
+        if integer < 2
+            puts "Be sure to account for negative numbers!"
+        return false
+        end
+        
+        array.each do |num|
+         if
+            integer % num == 0
+            return false
+         end
+        end
+
+    return true
+end
